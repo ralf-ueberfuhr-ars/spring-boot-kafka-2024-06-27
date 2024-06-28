f4df47b020fdda1ea3ca06f19aa2799efb9fdea5
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-06-28 23:29:11 +0200

    Fix DLT configuration.

--- statistics-service-provider/src/main/java/de/sample/schulung/statistics/kafka/CustomerRecord.java
+++ statistics-service-provider/src/main/java/de/sample/schulung/statistics/kafka/CustomerRecord.java
@@ -1,9 +1,15 @@
 package de.sample.schulung.statistics.kafka;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 import java.time.LocalDate;
 
 public record CustomerRecord(
+  @NotNull
   LocalDate birthdate,
+  @NotNull
+  @Pattern(regexp = "active|locked|disabled")
   String state
 ) {
 }
